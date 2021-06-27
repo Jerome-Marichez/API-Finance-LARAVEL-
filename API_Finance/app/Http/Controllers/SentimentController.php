3cf3915479cbc7c2cfa94410957f234177dbca3c
Jérôme MARICHEZ <62617561+digitstudio@users.noreply.github.com>
2021-06-27 15:43:17 +0200

    Moving to Laravel
    Moviing the project to laravel to make a true API Finance

---
+++ API_Finance/app/Http/Controllers/SentimentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SentimentController extends Controller
+{
+    public function getSentiment() {
+        return "test";
+    }
+}
